2b2419d61e9422bc117893ddc5e951a6baaf05d3
Tom Piscitell <tom.piscitell@gmail.com>
2013-12-22 18:24:17 -0500

    add proxy support for rvm_system install

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,17 @@
-class rvm($version=undef, $install_rvm=true) {
+class rvm($version=undef, $install_rvm=true, $proxy=undef) {
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
-    class {
-      'rvm::dependencies': stage => 'rvm-install';
-      'rvm::system':       stage => 'rvm-install', version => $version;
+    if $proxy {
+      class {
+        'rvm::dependencies': stage => 'rvm-install';
+        'rvm::system':       stage => 'rvm-install', version => $version, proxy => $proxy;
+      } 
+    } else {
+      class {
+        'rvm::dependencies': stage => 'rvm-install';
+        'rvm::system':       stage => 'rvm-install', version => $version;
+      }
     }
   }
 }
--- manifests/system.pp
+++ manifests/system.pp
@@ -1,4 +1,4 @@
-class rvm::system($version=undef) {
+class rvm::system($version=undef, $proxy=undef) {
 
   $actual_version = $version ? {
     undef     => 'latest',
@@ -6,12 +6,20 @@ class rvm::system($version=undef) {
     default   => $version,
   }
 
+  if $proxy {
+    $cmd = "bash -c '/usr/bin/curl --proxy ${proxy} -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
+            chmod +x /tmp/rvm-installer && \
+            rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
+            rm /tmp/rvm-installer'"
+  } else {
+    $cmd = "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
+            chmod +x /tmp/rvm-installer && \
+            rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
+            rm /tmp/rvm-installer'"
+  }
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
-                chmod +x /tmp/rvm-installer && \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
-                rm /tmp/rvm-installer'",
+    command => $cmd,
     creates => '/usr/local/rvm/bin/rvm',
     require => [
       Class['rvm::dependencies'],
