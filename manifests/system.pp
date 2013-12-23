5b4d1eb58b55fdfe785ff6cbd344c1f625725ef0
Tom Piscitell <tom.piscitell@gmail.com>
2013-12-23 15:29:29 -0500

    add proxy config for rvm_installer

--- manifests/system.pp
+++ manifests/system.pp
@@ -9,7 +9,7 @@ class rvm::system($version=undef, $proxy=undef) {
   if $proxy {
     $cmd = "bash -c '/usr/bin/curl --proxy ${proxy} -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
             chmod +x /tmp/rvm-installer && \
-            rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
+            rvm_proxy=${proxy} rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
             rm /tmp/rvm-installer'"
   } else {
     $cmd = "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
