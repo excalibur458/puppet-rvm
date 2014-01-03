dc5749033f68f2583e7bf363dc37f725476d2160
Tom Piscitell <tom.piscitell@gmail.com>
2014-01-03 13:50:36 -0500

    add workaround for bug in rvm ruby installer

--- manifests/system.pp
+++ manifests/system.pp
@@ -11,6 +11,15 @@ class rvm::system($version=undef, $proxy=undef) {
             chmod +x /tmp/rvm-installer && \
             rvm_proxy=${proxy} rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
             rm /tmp/rvm-installer'"
+
+    # Customize rvm_gem_options in /etc/rvmrc. This is a workaround for the fact
+    # that RVM does not pass the value of --proxy to gem when installing 
+    # the default gemset during an installation of ruby.
+    file { '/etc/rvmrc':
+      ensure  => present,
+      content => "umask u=rwx,g=rwx,o=rx\nexport rvm_gem_options=\"--no-rdoc --no-ri --http-proxy ${proxy}\"",
+      require => Exec['system-rvm'],
+    }
   } else {
     $cmd = "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
             chmod +x /tmp/rvm-installer && \
@@ -21,6 +30,7 @@ class rvm::system($version=undef, $proxy=undef) {
     path    => '/usr/bin:/usr/sbin:/bin',
     command => $cmd,
     creates => '/usr/local/rvm/bin/rvm',
+    timeout => 3000,
     require => [
       Class['rvm::dependencies'],
     ],
